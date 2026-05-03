d813a29f088c450b1aa2f4598c0ad16b34c83f00
franz <franzrobertmesina@gmail.com>
2026-05-03 21:22:21 +0800

    why the hell did i slightly struggle with this... im cooked

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let final = ""
+    for(let i = string.length - 1; i >= 0; i--)
+    {
+        final += string[i];
+    }
+    return final;
 };
 
 // Do not edit below this line
